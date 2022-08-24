953ccfc6f227d1b037dbe611f1fd2854032be0e7
Rohit Kumar <49610045+Rohit20ek@users.noreply.github.com>
2022-08-24 07:42:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# which is greater 
+a = input('enter first number: ')
+b = input('enter second number: ')
+
+if b>a:
+  print('b is greater than ' +str(a))
+else:
+  print('a is grater than' +str(b))
